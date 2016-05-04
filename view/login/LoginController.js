38f0291e74c9dd3ba014ace22c955fcfe9d4efe2
glenr4 <glenrutherford4@gmail.com>
2016-05-04 12:30:45 +1000

    Login user details check works with static data

--- view/login/LoginController.js
+++ view/login/LoginController.js
@@ -3,23 +3,45 @@
 
     alias: 'controller.login',
 
-    onLoginClick: function() {
+    onLoginClick: function () {
+        console.log('LoginController: onLoginClick');
+        //Check user credentials
+        var store = Ext.data.StoreManager.lookup('userstore');
+        console.log(store);
 
-        // This would be the ideal location to verify the user's credentials via
-        // a server-side lookup. We'll just move forward for the sake of this example.
+        var userName = 'whodat';
+        var password = '123';
 
-        // Set the localStorage value to true
-        localStorage.setItem("loginValid", true);
-
-        // Remove Login Window
-        this.getView().destroy();
-
-        // Add the main view to the viewport
-        Ext.create({
-            xtype: 'app-main'
+        var match = store.findBy(function (record, id) {
+            if (record.get('userName') == userName &&
+                record.get('password') == password) {
+                console.log('match: true');
+                return true;
+            };
         });
+        console.log(match);
 
+        if (match != -1) {
+            console.log('if match true');
+            // Set the localStorage value to true
+            localStorage.setItem("loginValid", true);
+
+            // Remove Login Window
+            this.getView().destroy();
+
+            // Add the main view to the viewport
+            Ext.create({
+                xtype: 'app-main'
+            });
+        } else {
+            console.log('if match false');
+            Ext.Msg.show({
+                title: 'Login Details Not Valid',
+                message: 'Please check your user name and password and try again.',
+                buttons: Ext.Msg.OK,
+                icon: Ext.Msg.WARNING,
+            });
+        };
     }
-    
 });
 
